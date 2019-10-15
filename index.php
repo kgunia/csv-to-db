9d3305ad23205a6657a5d25aaf37111645e585ec
Karol Gunia <kgunia@gmail.com>
2019-10-16 01:00:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,32 @@
 <?php
 	/**
-	 * Script name: csv-to-db
-	 * Script URI: https://github.com/kgunia/csv-to-db
-	 * Author: Karol Gunia
-	 * Author URI: https://kgunia.pl
-	 * Description: Import data from CSV to MySQL database, developed for CSI S.A. 
-	 * Version: 1.0.0
-	 * License: MIT 
-	 **/
+	 * Copyright (c) 2019 Karol Gunia <biuro@kgunia.pl>
+	 *
+	 * Permission is hereby granted, free of charge, to any person obtaining a copy
+	 * of this software and associated documentation files (the "Software"), to deal
+	 * in the Software without restriction, including without limitation the rights
+	 * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	 * copies of the Software, and to permit persons to whom the Software is
+	 * furnished to do so, subject to the following conditions:
+	 *
+	 * The above copyright notice and this permission notice shall be included in
+	 * all copies or substantial portions of the Software.
+	 * 
+	 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	 * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	 * THE SOFTWARE.
+	 * 
+	 * @author      Karol Gunia
+	 * @copyright   2019 (c) Karol Gunia
+	 * @license     http://www.opensource.org/licenses/mit-license
+	 * @link        http://github.com/kgunia/csv-to-db
+	 * @package     csv-to-db
+	 * @version     1.0.0
+	 */
 
 	// DB configuration
 	$db_host	=	"localhost"; 				// DB host
